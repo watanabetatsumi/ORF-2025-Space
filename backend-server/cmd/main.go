a414b95a2a6e1c25a5779f00460f96f39e9aa2c9
watanabetatsumi <tatsumi@tatsumi>
2025-11-13 16:14:52 +0900

    second commit

--- backend-server/cmd/main.go
+++ backend-server/cmd/main.go
@@ -1,5 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/watanabetatsumi/backend-server/intenal/service"
+)
+
 func main() {
 	r := gin.Default()
+
+	bpsrv := service.NewBackendService()
 
 	r.Run()
 }
--- backend-server/internal/application/service/bp_service.go
+++ backend-server/internal/application/service/bp_service.go
@@ -7,3 +7,13 @@ import (
 type BpService struct {
 	bpgateway gateway.BpGateway
 }
+
+func NewBpService(bpgateway gateway.BpGateway) *BpService {
+	return &BpService{
+		bpgateway: bpgateway,
+	}
+}
+
+func (bs *BpService) GetContent(domain string) (string, error) {
+	return bs.bpgateway.GetContent(domain)
+}
